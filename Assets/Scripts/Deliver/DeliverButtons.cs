ff2d0cb6e408957b4424a4d9e2e96c9da7419a03
kameringring1029 <kameringring1029@gmail.com>
2020-07-11 23:25:21 +0900

    imple accel-decel

--- Assets/Scripts/Deliver/DeliverButtons.cs
+++ Assets/Scripts/Deliver/DeliverButtons.cs
@@ -13,4 +13,17 @@ public class DeliverButtons : MonoBehaviour
 	{
         DeliverGameManager.Instance.trgBrake();
 	}
+
+    public void onClickRide()
+    {
+        switch (DeliverGameManager.Instance.rider_state)
+        {
+            case DeliverGameManager.RIDE.RIDE:
+                DeliverGameManager.Instance.changeRiderState(DeliverGameManager.RIDE.WALK);
+                break;
+            case DeliverGameManager.RIDE.WALK:
+                DeliverGameManager.Instance.changeRiderState(DeliverGameManager.RIDE.RIDE);
+                break;
+        }
+    }
 }
--- Assets/Scripts/Deliver/DeliverGameManager.cs
+++ Assets/Scripts/Deliver/DeliverGameManager.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 
 public class DeliverGameManager : SingletonMonoBehaviour<DeliverGameManager>
 {
+    public enum RIDE { RIDE, WALK };
+    public RIDE rider_state = RIDE.RIDE;
+
     public float speed { get; private set; } //今のスピード
     bool flg_break; //ブレーキかけてるフラグ
 
@@ -14,7 +17,7 @@ public class DeliverGameManager : SingletonMonoBehaviour<DeliverGameManager>
     void Start()
     {
         flg_break = false;
-        speed = speed_max;
+        speed = 0;
     }
 
     // Update is called once per frame
@@ -32,15 +35,29 @@ public class DeliverGameManager : SingletonMonoBehaviour<DeliverGameManager>
 
         switch (flg_break)
         {
-            case true: //減速
-                if (speed <= 0) { speed = 0; break; }
-                if (speed > 0f) speed -= speed_acce;
+            case true: /*減速時*/
+
+                //
+                if (speed <= 0) {
+                    speed = 0;
+                    break;
+                }
+
+                // 減速
+                if (speed > 0f) speed -= speed_acce*2;
+
                break;
 
-            case false: //加速
+            case false: /*加速時*/
+                
+                // 加速
                 if (speed < speed_max) speed += speed_acce;
+
                 break;
         }
+
+
+        DeliverRider.Instance.GetComponent<Animator>().SetFloat("speed", speed);
 
     }
 
@@ -49,5 +66,41 @@ public class DeliverGameManager : SingletonMonoBehaviour<DeliverGameManager>
      */
     public void trgBrake(){
         flg_break = !flg_break;
+
+        switch (flg_break)
+        {
+            case true:
+                DeliverRider.Instance.GetComponent<Animator>().SetBool("isBreaking", true);
+                break;
+            case false:
+                DeliverRider.Instance.GetComponent<Animator>().SetBool("isBreaking", false);
+                break;
+        }
+    }
+
+    /*
+     *
+     */
+    public void changeRiderState(RIDE state)
+    {
+        if (state == rider_state) return; // 今のstateと変化なければなにもしない
+
+        switch (state)
+        {
+            case RIDE.RIDE:
+                Debug.Log("change to Ride");
+                speed_max = 2.0f;
+                speed_acce = 0.01f;
+                DeliverRider.Instance.GetComponent<Animator>().SetInteger("state",0);
+                break;
+            case RIDE.WALK:
+                Debug.Log("change to walk");
+                speed_max = 0.5f;
+                speed_acce = 0.003f;
+                DeliverRider.Instance.GetComponent<Animator>().SetInteger("state", 1);
+                break;
+        }
+
+        rider_state = state;
     }
 }
